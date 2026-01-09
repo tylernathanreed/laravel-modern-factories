b564fd360b504a632c0f593d44ca6678a8bfa680
Tyler Reed <tylernathanreed@gmail.com>
2026-01-09 22:10:12 +0000

    Initial Commit

---
+++ .php-cs-fixer.php
@@ -0,0 +1,51 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+/**
+ * This is a shim to use Laravel Pint syntax on older versions of PHP.
+ * 
+ * @link https://github.com/laravel/pint
+ */
+
+$basePath = rtrim(realpath(__DIR__), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+
+$overridePath = $basePath . 'pint.override.json';
+
+$configPath = file_exists($overridePath)
+    ? $overridePath
+    : $basePath . 'pint.json';
+
+if (! file_exists($configPath)) {
+    throw new RuntimeException("The configuration file [{$configPath}] is missing.");
+}
+
+$pintConfig = json_decode(file_get_contents($configPath), true);
+
+if (! is_array($pintConfig)) {
+    throw new RuntimeException("The configuration file [{$configPath}] is not valid JSON.");
+}
+
+$finder = Finder::create()
+    ->in(getcwd())
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+if (isset($pintConfig['exclude'])) {
+    $finder->exclude($pintConfig['exclude']);
+}
+
+if (isset($pintConfig['notPath'])) {
+    $finder->notPath($pintConfig['notPath']);
+}
+
+if (isset($pintConfig['notName'])) {
+    $finder->notName($pintConfig['notName']);
+}
+
+return (new Config)
+    ->setFinder($finder)
+    ->setRules($pintConfig['rules'] ?? [])
+    ->setRiskyAllowed(true)
+    ->setUsingCache(true);
---
+++ shim/Database/Eloquent/Factories/Sequence.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Illuminate\Database\Eloquent\Factories;
+
+use Closure;
+use Countable;
+
+class Sequence implements Countable
+{
+    /**
+     * The sequence of return values.
+     *
+     * @var array
+     */
+    protected $sequence;
+
+    /**
+     * The count of the sequence items.
+     *
+     * @var int
+     */
+    public $count;
+
+    /**
+     * The current index of the sequence iteration.
+     *
+     * @var int
+     */
+    public $index = 0;
+
+    /**
+     * Create a new sequence instance.
+     *
+     * @param  array  $sequence
+     * @return void
+     */
+    public function __construct(...$sequence)
+    {
+        $this->sequence = $sequence;
+        $this->count = count($sequence);
+    }
+
+    /**
+     * Get the current count of the sequence items.
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+        return $this->count;
+    }
+
+    /**
+     * Get the next value in the sequence.
+     *
+     * @return mixed
+     */
+    public function __invoke()
+    {
+        $value = $this->sequence[$this->index % $this->count];
+
+        if ($value instanceof Closure) {
+            $value = $value($this);
+        }
+
+        $this->index = $this->index + 1;
+
+        return $value;
+    }
+}
